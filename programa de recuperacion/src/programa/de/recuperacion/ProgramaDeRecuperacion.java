8b8b275eaef50ff752fc5a89c36f45202c17f0a2
Evelyn Guangasig <evelyn.guangasig9148@utc.edu.ec>
2026-06-04 22:43:08 -0500

    espacio

--- programa de recuperacion/src/programa/de/recuperacion/ProgramaDeRecuperacion.java
+++ programa de recuperacion/src/programa/de/recuperacion/ProgramaDeRecuperacion.java
@@ -16,6 +16,7 @@ public class ProgramaDeRecuperacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         try ( // codigo paracalcular notas
         java.util.Scanner teclado = new Scanner(System.in)) {
             System.out.println("=== SISTEMA DE NOTAS INTERACTIVO ===");
