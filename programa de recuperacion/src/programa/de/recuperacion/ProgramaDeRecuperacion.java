522dd98aa18d386ecd784b30a643b4c85332a584
Evelyn Guangasig <evelyn.guangasig9148@utc.edu.ec>
2026-06-04 22:59:11 -0500

    pantalla

--- programa de recuperacion/src/programa/de/recuperacion/ProgramaDeRecuperacion.java
+++ programa de recuperacion/src/programa/de/recuperacion/ProgramaDeRecuperacion.java
@@ -32,7 +32,7 @@ public class ProgramaDeRecuperacion {
             double nota3 = teclado.nextDouble();
             // 4. Calcular el promedio de las tres notas
             double promedio = (nota1 + nota2 + nota3) / 3;
-            // 5. Mostrar los resultados finales en la pantalla
+            // 5. Mostrar los resultados finales en la pantalla principal
             System.out.println("\n--- RESULTADOS PARA: " + nombre.toUpperCase() + " ---");
             System.out.printf("El promedio final es: %.2f\n", promedio);
             // 6. Decidir si aprobó o reprobó usando un condicional simple
